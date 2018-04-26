092895c91adcf6bb73860ce64dd36c1f63a6b756
Ilidio Lopes <depil01@ca.com>
2018-04-26 16:51:16 +0200

    12. The Execution Context - Code Execution

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+function b() {
+    console.log('Called b!');
+}
+
+b();
 
 console.log(a);
 
-if (a === undefined) {
-    console.log('a is undefined.');
-} else {
-    console.log('a is defined.');
-}
+var a = 'Hello World!'
+
+console.log(a);
